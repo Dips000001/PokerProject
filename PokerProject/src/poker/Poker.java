fd8b66e3dfab11b435a93f73dcdb6f42e9e4ba8d
Dips000001 <wailsuwn8-c@my.cityu.edu.hk>
2024-09-20 14:24:26 +0800

    Fxxk Git Twice

--- PokerProject/src/poker/Poker.java
+++ PokerProject/src/poker/Poker.java
@@ -20,30 +20,35 @@ return isThreeOfaKind(cards, n) && isTwoPairs(cards, n);
 }
 // Precondition: A hand of n cards have been sorted by card number.
 public boolean isThreeOfaKind(String cards[], int n) {
-for (int i=0; i<n-2; i++) {
-if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
-cards[i+1].charAt(1) == cards[i+2].charAt(1))
-return true;
-}
-return false;
+	for (int i=0; i<n-2; i++) {
+		if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
+				cards[i+1].charAt(1) == cards[i+2].charAt(1))
+			return true;
+	}
+	return false;
 }
 // Precondition: A hand of n cards have been sorted by card number.
 public boolean isTwoPairs(String cards[], int n) {
-int count = 0;
-for (int i=0; i<n-1; i++) {
-if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
-count++;
-i++;
+	int count = 0;
+	for (int i=0; i<n-1; i++) {
+		if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+			count++;
+		}
+		else
+		{
+			if (count == 1)
+				return true;
+			count = 0;
+		}
+	}
+	if (count == 1)
+		return true;
+	return false;
 }
-}
-if (count == 2)
-return true;
-else
-return false;
-}
+
 public static void main(String args[]) {
-System.out.println(new Poker().isFullHouse(
-new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
-}
+	System.out.println(new Poker().isFullHouse(
+			new String[] {"C3", "D3", "S3", "HX", "SX"}, 5));
+	}
 }
 // end of Poker.java
